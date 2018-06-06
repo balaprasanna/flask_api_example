2cbe2ae0d6356dbb704d23175bb4466e79007caf
balaprasanna <balaprasannav2009@gmail.com>
2018-06-06 13:10:52 +0700

    App finished

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-slack_token = "xoxp-373219160785-374655318694-377498875543-54c303547e1d2dae997eb6a7509a48cd"
+slack_token = "xoxp-373219160785-374655318694-376231293956-a6532fb79f77b6a1fa1ece2ed374d5db"
 from slackclient import SlackClient
 
 # Explain later
@@ -7,7 +7,38 @@ from slackclient import SlackClient
 # Utility Functions:
 def send_message_to_slack(channel, message):
     sc = SlackClient(slack_token)
+    print ("Sending message ??")
+    print (channel, message)
     sc.api_call( "chat.postMessage", channel=channel, text=message )
 
 
 # Main code starts here:
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/sendmessage', methods=['GET'])
+def postMessage():
+    text = request.args.get('text')
+    print (text)
+    channel = "general"
+    send_message_to_slack(channel, text)
+    return "Message sent successfully : {}".format(text)
+
+
+@app.route('/sendmessage_to_channel', methods=['GET'])
+def postMessagetoAChannel():
+    channel = request.args.get('channel')
+    text = request.args.get('text')
+    
+    send_message_to_slack(channel, text)
+    return "Message sent successfully to {} channel".format(channel)
+
+# to make sure everyone can access
+app.run(host= '0.0.0.0')
